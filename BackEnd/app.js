466a93f9d5193721614a81f87cbf3ef784ae9bae
Anteater <zx1056@naver.com>
2020-03-07 13:02:36 +0900

    schemas with mongoose

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -23,8 +23,8 @@ require('./socket')(server);
 server.listen(port, () => {
     console.log(`Example app listening on ${port} port!`)
     
-    // let testLine = new database.models.Lines({name: 'jokes', text: '열심히 공부중'});
-    // testLine.save((err) => {
+    // let testData1 = new database.models.Channels({channelName: 'jokes01'});
+    // testData1.save((err) => {
     //     if(err) {
     //         console.log('error 발생');
     //         console.log('-------------------------------');
@@ -32,7 +32,7 @@ server.listen(port, () => {
     //         console.log('-------------------------------');
     //     }
     //     else {
-    //         console.log("테스트용 데이터 추가됨 : " + testLine.collection.collectionName);
+    //         console.log("테스트용 데이터 추가됨 : " + testData1.collection.collectionName);
     //     }
 
     //     database.models.Channels.findAll((err, channels) => {
@@ -41,6 +41,36 @@ server.listen(port, () => {
     //         else console.log(channels);
     //     });
     // });
+
+    // let testData2 = new database.models.Users({userName: 'anteater01'});
+    // testData2.save((err) => {
+    //     if(err) {
+    //         console.log('error 발생');
+    //         console.log('-------------------------------');
+    //         console.error(err);
+    //         console.log('-------------------------------');
+    //     }
+    //     else {
+    //         console.log("테스트용 데이터 추가됨 : " + testData2.collection.collectionName);
+    //     }
+
+    //     database.models.Users.findAll((err, channels) => {
+    //         if(err) console.log(err);
+    //         else if(channels.length === 0) console.log('no users here');
+    //         else console.log(channels);
+    //     });
+    // });
+
+    database.models.Users.find({userName:'anteater01'})
+    .then((docs) => {
+        if (docs.length === 0) console.log("no such user here");
+        else {
+            console.log(docs[0].userName);
+        }
+    })
+    .catch(err => {
+        console.error(err);
+    });
 });
 
 module.exports = app;
