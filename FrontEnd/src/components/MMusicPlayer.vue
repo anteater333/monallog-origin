2c0743b5e2b16763470ae25c4c152755821fbd83
Anteater <zx1056@naver.com>
2020-04-03 19:02:22 +0900

    Revert "playlist가 없는 채널 접근 시 오류 수정"
    This reverts commit c3feb9080269ff794921f3a6f67a4f3eb12cc735.

--- FrontEnd/src/components/MMusicPlayer.vue
+++ FrontEnd/src/components/MMusicPlayer.vue
@@ -20,47 +20,54 @@ export default {
     },
     data () {
         return {
-            index: 0
+            index: 0,
+            playingOrder: Array
         }
     },
     computed: {
-        playingOrder: function () {
-            let order = [...Array(this.playlist.length).keys()]
-            if (this.mode.isRandom) {
-                order = shuffle(order)
-            }
-            return order
-        },
         nowPlaying: function () {
-            const URL = (this.playingOrder !== undefined) ? this.playlist[this.playingOrder[this.index]] : ''
-            return URL
+            const target = (this.playingOrder !== undefined) ? this.playingOrder[this.index] : this.index
+            return this.playlist[target].URL
         },
         hasMusic: function () {
-            if (this.playlist.length === 0) {
+            if (!this.playlist) {
                 return false
             }
             return true
         }
     },
     created () {
-
+        this.playingOrder = [...Array(this.playlist.length).keys()]
+        if (this.mode.isRandom) {
+            this.shuffle()
+        }
     },
     methods: {
         nextTrack: function () {
             const ctrl = this.$refs.audioCtrl
 
-            this.index = (this.index + 1) % this.playlist.length
+            this.index += 1
+
+            if (this.index >= this.playlist.length) {
+                this.index = this.index % this.playlist.length
+                if (!this.mode.isLoop) {
+                    this.shuffle()
+                    ctrl.load()
+                    return ctrl.pause()
+                }
+            }
+
             ctrl.load()
             ctrl.play()
+        },
+        shuffle: function () {
+            this.playingOrder = this.playingOrder
+                .map(a => [Math.random(), a])
+                .sort((a, b) => a[0] - b[0])
+                .map(a => a[1])
         }
     }
 }
-
-const shuffle = arr => arr
-    .map(a => [Math.random(), a])
-    .sort((a, b) => a[0] - b[0])
-    .map(a => a[1])
-
 </script>
 
 <style scoped>
